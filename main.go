fc8978ea635f7b08c818a7d0c7c56eacd9d96d89
zmanxiang <zmanxiang@fortinet.com>
2021-03-16 18:03:44 -0700

    ADD: main.go

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	nfqueue "github.com/florianl/go-nfqueue"
+	"time"
+)
+
+func main() {
+	config := nfqueue.Config{
+		NfQueue:      100,
+		MaxPacketLen: 0xFFFF,
+		MaxQueueLen:  0xFF,
+		Copymode:     nfqueue.NfQnlCopyPacket,
+		ReadTimeout:  10 * time.Millisecond,
+		WriteTimeout: 15 * time.Millisecond,
+	}
+
+	nf, err := nfqueue.Open(&config)
+	if err != nil {
+		fmt.Println("could not open nfqueue socket:", err)
+		return
+	}
+	defer nf.Close()
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	fn := func(a nfqueue.Attribute) int {
+		id := *a.PacketID
+		// Just print out the id and payload of the nfqueue packet
+		fmt.Printf("[%d]\t%v\n", id, *a.Payload)
+		nf.SetVerdict(id, nfqueue.NfAccept)
+		return 0
+	}
+
+	// Register your function to listen on nflqueue queue 100
+	err = nf.Register(ctx, fn)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	// Block till the context expires
+	<-ctx.Done()
+}
